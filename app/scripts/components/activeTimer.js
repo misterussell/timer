7307ee3331aac99cab5e94f1cfb48513042d1a32
Maxwell Russell <maxruss.87@gmail.com>
2016-12-09 16:31:09 -0600

    updating mobile styling for active timer

--- app/scripts/components/activeTimer.js
+++ app/scripts/components/activeTimer.js
@@ -23,8 +23,7 @@ export default React.createClass({
     let timer, count;
     if (store.timers.toJSON().length < 1) {
       // condition check to verify if data loaded
-      this.fetch({ url: `https://api.backendless.com/v1/data/Timers/${this.props.params.id}`});
-      // this.loadData();
+      this.loadData();
     } else if (this.props.groupTemplate || this.props.mobilityTemplate) {
       // condition check to verify if part of group of timers, if is sent in with time constraint if will calculate when you need to leave
       timer = this.props.timer;
