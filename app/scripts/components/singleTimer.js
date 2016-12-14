633a5ef25019320c110855b2e634431206d8d4bf
Maxwell Russell <maxruss.87@gmail.com>
2016-12-14 14:56:32 -0600

    updating display of single timer to include seconds

--- app/scripts/components/singleTimer.js
+++ app/scripts/components/singleTimer.js
@@ -8,6 +8,7 @@ import store from '../store';
 export default React.createClass({
   render() {
     let note = null;
+    let timerValue = `${ Math.floor(this.props.timer.timerValue)} minutes`;
 
     if (this.props.timer.origin && this.props.timer.destination) {
       note = `${this.props.timer.origin} to ${this.props.timer.destination}`;
@@ -17,10 +18,14 @@ export default React.createClass({
       note = this.props.timer.note;
     }
 
+    if (Number(this.props.timer.timerValue) < 1) {
+      timerValue = `${this.props.timer.timerValue * 60} seconds`;
+    }
+
     return (
       <li className="timer-template">
         <h2 className="timer-name">{ this.props.timer.name }</h2>
-        <h3 className="timer-value">{ this.props.timer.timerValue } minutes</h3>
+        <h3 className="timer-value">{ timerValue } </h3>
         <h4 className="timer-note">
           { note }
         </h4>
